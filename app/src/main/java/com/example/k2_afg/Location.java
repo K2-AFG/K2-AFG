50a425f91da772825d33de6e3ce9ebeeae3bf26d
willhung <willhung2003@gmail.com>
2020-04-20 22:36:04 -0400

    hihi

---
+++ app/src/main/java/com/example/k2_afg/Location.java
@@ -0,0 +1,60 @@
+package com.example.k2_afg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.w3c.dom.Text;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class Location extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Button button = findViewById(R.id.getLocation);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if (ActivityCompat.checkSelfPermission(Location.this, ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+
+                client.getLastLocation().addOnSuccessListener(Location.this, new OnSuccessListener<android.location.Location>() {
+                    @Override
+                    public void onSuccess(android.location.Location location) {
+                        if(location!=null){
+                            TextView textView = findViewById(R.id.Location);
+                            textView.setText(location.toString());
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
+    }
+
+}
