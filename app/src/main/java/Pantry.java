ee197d0436fe327aa2f5c15b417ac1d20f21fd86
neena <ndxiang@wpi.edu>
2020-04-21 20:31:20 -0400

    Organization, Pantry, and Shelter objects updated. Food item also added

--- app/src/main/java/Pantry.java
+++ app/src/main/java/Pantry.java
@@ -1,25 +1,26 @@
+import java.util.ArrayList;
 
 public class Pantry extends Organization {
 
 	/**
-	 * A string containing all the types of food a shelter currently has
+	 * an array list containing information about all the foods types a pantry holds
 	 */
-	private String foodType;
-	
+	private ArrayList<FoodItem> foods;
+
 	/**
-	 * the amount food a pantry has
+	 * the number of different types of foods a pantry has
 	 */
-	private int numFoods;
-	
+	private int variety;
+
 	/**
 	 * creates a pantry with no name, address, phone number, website, description, food types, or amount of food
 	 */
 	public Pantry() {
 		super();
-		foodType = "";
-		numFoods = 0;
+		foods = new ArrayList<FoodItem>();
+		variety = 0;
 	}
-	
+
 	/**
 	 * creates a pantry with a name, address, phone number, website, description, food types, and amount of food
 	 * @param name the name of the pantry
@@ -27,46 +28,90 @@ public class Pantry extends Organization {
 	 * @param phoneNum the phone number of the pantry
 	 * @param website the link to the website of the pantry
 	 * @param description a brief description of the pantry
-	 * @param numFoods the amount of food a pantry has
-	 * @param foodType the types of food a pantry has
+	 * @param longitude the longitude of the pantry
+	 * @param latitude the latitude of the pantry
 	 */
-	public Pantry (String name, String address, int phoneNum, String website, String description, 
-			int numFoods, String foodType) {
-		super(name, address, phoneNum, website, description);
-		this.numFoods = numFoods;
-		this.foodType = foodType;
+	public Pantry (String name, String address, int phoneNum, String website, String description, double longitude, double latitude) {
+		super(name, address, phoneNum, website, description, longitude, latitude);
+		foods = new ArrayList<FoodItem>();
+		variety = 0;
 	}
 
 	/**
-	 * gets the types of food a pantry holds
-	 * @return the types of food a pantry holds
+	 * adds a new amount of a food item if it already exists in the foods array list or adds a new food item if it isn't there
+	 * @param foodName the name of the food item to add an amount to
+	 * @param amount the amount of a food type to add
 	 */
-	public String getFoodType() {
-		return foodType;
+	public void add(String foodName, double amount) {
+		boolean repeated = false;
+		for (int i = 0; i < foods.size(); i++) {
+			if (foodName.equals(foods.get(i).getType())) {
+				foods.set(i, new FoodItem(foodName, foods.get(i).getAmount() + amount));
+				repeated = true;
+			}
+		}
+
+		if(!repeated) {
+			foods.add(new FoodItem(foodName, amount));
+
+		}
+
+		variety = foods.size();
 	}
 
 	/**
-	 * sets the types of food a pantry has to the these types of food
-	 * @param foodType the types of food to set
+	 * replaces the amount of a existing food item or adds it to the end of the list if it doesn't exist
+	 * @param foodName the name of the food item
+	 * @param amount the new amount of the food item to replace
 	 */
-	public void setFoodType(String foodType) {
-		this.foodType = foodType;
+	public void replace(String foodName, double amount) {
+		boolean repeated = false;
+		for (int i = 0; i < foods.size(); i++) {
+			if (foodName.equals(foods.get(i).getType())) {
+				foods.set(i, new FoodItem(foodName, amount));
+				repeated = true;
+			}
+		}
+
+		if(!repeated) {
+			foods.add(new FoodItem(foodName, amount));
+
+		}
+
+		variety = foods.size();
+	}
+
+
+	/**
+	 * gets the foods array list of the pantry
+	 * @return the foods array list to get
+	 */
+	public ArrayList<FoodItem> getFoods() {
+		return foods;
 	}
 
 	/**
-	 * gets the amount of food a pantry has
-	 * @return the amount of food a pantry has
+	 * sets the foods array list to this array list of food items
+	 * @param foods the array list of food items to set
 	 */
-	public int getNumFoods() {
-		return numFoods;
+	public void setFoods(ArrayList<FoodItem> foods) {
+		this.foods = foods;
 	}
 
 	/**
-	 * sets the amount of food to this amount
-	 * @param numFoods the amount of food to be set
+	 * gets the number of types of foods a pantry has
+	 * @return the number of variety of foods a pantry has
 	 */
-	public void setNumFoods(int numFoods) {
-		this.numFoods = numFoods;
+	public int getVariety() {
+		return variety;
 	}
-	
+
+	/**
+	 * sets the number of varieties of foods a pantry has
+	 * @param variety the number of different types of foods
+	 */
+	public void setVariety(int variety) {
+		this.variety = variety;
+	}
+
 }
