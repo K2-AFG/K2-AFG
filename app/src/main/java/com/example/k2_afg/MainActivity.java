90b4c9258a3e6d9fd0468ab8a5c4637ad1a06504
sjeyaram123 <sjeyaram@wpi.edu>
2020-04-26 00:13:26 -0400

    Search Feature

--- app/src/main/java/com/example/k2_afg/MainActivity.java
+++ app/src/main/java/com/example/k2_afg/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.k2_afg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,36 +12,81 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class MainActivity extends AppCompatActivity {
+//    private Scanner x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+//        myRef.setValue("Hello, World!");
+
+        Log.v("myTag", "Here");
 
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("MainActivity", "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("MainActivity", "Failed to read value.", error.toException());
-            }
-        });
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d("MainActivity", "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("MainActivity", "Failed to read value.", error.toException());
+//            }
+//        });
     }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        InputStream is = getResources().openRawResource(R.raw.shelters);
+//        String searchTerm = "Abby's House";
+//        readRecord(searchTerm, is);
+//    }
 
-
+//    public void readRecord(String searchTerm, InputStream is) {
+//        //BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        boolean found = false;
+//        String line = "";
+//        String name = "";
+//        String address = "";
+//        String number = "1";
+//
+//        try {
+//            x = new Scanner(is);
+//            x.useDelimiter("[,\n]");
+//            while(x.hasNext() && !found) {
+//                name = x.next();
+//                address = x.next();
+//                number = x.next();
+//
+//                if(name.equals(searchTerm)) {
+//                    found = true;
+//                }
+//            }
+//            if(found) {
+//                Log.v("MainActivity", "name " + name);
+//            } else {
+//                Log.v("MainActivity", "error ");
+//            }
+//        }
+//        catch(Exception e) {
+//
+//        }
+//    }
 }
--- app/src/main/java/com/example/k2_afg/searchpage.java
+++ app/src/main/java/com/example/k2_afg/searchpage.java
@@ -1,14 +1,81 @@
 package com.example.k2_afg;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Environment;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class searchpage extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter adapter;
+    ArrayList<String> shelters = new ArrayList<String>();
+    SearchView sv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchpage);
+
+        lv = (ListView) findViewById(R.id.searchpage);
+        sv = (SearchView) findViewById(R.id.search);
+        adapter = new ArrayAdapter(this,android.R.layout. simple_list_item_1, shelters);
+        lv.setAdapter(adapter);
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        readData();
+    }
+
+    private void readData() {
+        InputStream is = getResources().openRawResource(R.raw.shelters);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        try {
+            String value = reader.readLine();
+            while (value != null) {
+                Log.v("welcomeTag", value);
+                shelters.add(value);
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            Log.wtf("welcomeTag", "Error reading data on line");
+        }
     }
 }
+
+
